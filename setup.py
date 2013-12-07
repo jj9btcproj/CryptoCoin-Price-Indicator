65547bd0222c2df31b06d5edf07b8a7f4606a30c
jj9btcproj <legionaire1fff@gmail.com>
2013-12-06 20:05:04 -0500

    Update setup.py
    clear old settings in new install

--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ if removePack:
     print 'Removed files.'
 else:
     dirIn = str(raw_input( "Basic Setup (Input anything other than *advanced* for basic setup):"))
+    os.remove(SETTINGSFILE)
     if not  'dvanced' in dirIn:
         dirApp = os.getcwd()
         dirIn = dirApp
